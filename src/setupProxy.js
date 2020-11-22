694f40714552b63f19c853e15000e19b7fb32aa9
Siddhartha Shukla <siddhartha.shukla.cer16@itbhu.ac.in>
2020-11-23 02:25:35 +0530

    check

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,4 +1,4 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function(app){
-	app.use(createProxyMiddleware('/get_symptoms', {target: 'https://disease-pred.herokuapp.com'}));
+	app.use(createProxyMiddleware('/get_symptoms', {target: 'https://disease-pred.herokuapp.com/'}));
 }
